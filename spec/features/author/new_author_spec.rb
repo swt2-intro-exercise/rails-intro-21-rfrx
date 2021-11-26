7e8b3721d077fb4389b3fdfc7711c23a0ea5691f
rfrx <rieke.freund@student.hpi.de>
2021-11-26 11:59:57 +0100

    fixes #6

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -12,5 +12,13 @@ require 'rails_helper'
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+  it "should save the author" do 
+  visit new_author_path
+  page.fill_in 'author[first_name]', with: 'Alan'
+  page.fill_in 'author[last_name]', with: 'Turing'
+  page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
   
+  find('input[type="submit"]').click
+
+  end
 end
