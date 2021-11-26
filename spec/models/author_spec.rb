6f4d55d804a90ff4ea53e8a29b88ba3b2c7c3e87
rfrx <rieke.freund@student.hpi.de>
2021-11-26 11:31:17 +0100

    fixed #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-it "shpuld have first name, last name, and homepage" do
+it "should have first name, last name, and homepage" do
   author = Author.new(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
   expect(author.first_name).to eq("Alan")
   expect(author.last_name).to eq("Turing")
+  expect(author.name).to eq("Alan Turing")
   expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   end
 end
