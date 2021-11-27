3b675779bce2595acf442dd31f4ceaf838bbf08a
rfrx <rieke.freund@student.hpi.de>
2021-11-27 15:07:24 +0100

    fixes #13

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -10,4 +10,10 @@ describe "Show author index page", type: :feature do
     expect(page).to have_link 'New', href: new_author_path
   end
 
+  it "should link to edit page" do
+    @author = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path(@author)
+  end
+
 end
